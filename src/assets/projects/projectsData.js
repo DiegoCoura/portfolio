21d877c70a0b73d8cc8c393b5e804bd9828d123f
DiegoCoura <diegogomes.10.9@gmail.com>
2023-07-25 00:46:20 -0300

    Fixed url cards banners

--- src/assets/projects/projectsData.js
+++ src/assets/projects/projectsData.js
@@ -2,7 +2,7 @@ const Projetos = [
     {
         id: 1,
         title: "Personal Portfolio",
-        banner: "src/assets/imgs/card-portfolio.png",
+        banner: "/static/images/card-portfolio.png",
         description: "This portfolio website was developed to put into practice my front-end knowledge, as well to showcase my projects and skills.",
         tecnologies: ["/static/images/react-100.svg", "/static/images/bootstrap-100.svg", "/static/images/css-100.svg"]
 
@@ -10,7 +10,7 @@ const Projetos = [
     {
         id: 2,
         title: "E-mail Marketing",
-        banner: "src/assets/imgs/card-emailmkt.png",
+        banner: "/static/images/card-emailmkt.png",
         description: "The project involves creating HTML and CSS for email marketing and developing the correct code for an email delivery platform.",
         tecnologies: ["/static/images/html-100.svg", "/static/images/css-100.svg"]
     }
