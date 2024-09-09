408a5e3953c0efb07cc8f6b0cf2341ce032cc81e
DiegoCoura <diegogomes.10.9@gmail.com>
2024-09-09 18:45:02 -0300

    Update: add new members-only project

--- src/assets/projects/projectsData.js
+++ src/assets/projects/projectsData.js
@@ -1,29 +1,29 @@
 const Projetos = [
     {
         id: 1,
+        title: "Members Only",
+        banner: "/static/images/membersonly.png",
+        description: "This Node/Express server-side rendering project, allow only authorized people to interact. The project includes an authentication system and the data is stored in a PostgreSQL database hosted on Railway.",
+        technologies: ["/static/images/nodejs.png", "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg", "/static/images/ejs.svg", "/static/images/postgresql.svg"],
+        live:"https://members-only-production-9476.up.railway.app/",
+        code: "https://github.com/DiegoCoura/members-only",
+    },
+    {
+        id: 2,
+        title: "Todo List",
+        banner: "/static/images/todo2.png",
+        description: "This is not the old same todo list that you see around, developed with vanilla javascript and css, also contains drag and drop feature, which takes this project to another level, allowing the users to change the tasks order.",
+        technologies: ["/static/images/html-100.svg", "/static/images/css-100.svg", "/static/images/js-100.svg"],
+        live:"https://diegocoura.github.io/todo-list/",
+        code:"https://github.com/DiegoCoura/todo-list"
+    }, {
+        id: 3,
         title: "Personal Portfolio",
         banner: "/static/images/card-portfolio.png",
         description: "This portfolio website was developed to put into practice my front-end knowledge, as well to showcase my projects and skills.",
         technologies: ["/static/images/react-100.svg", "/static/images/bootstrap-100.svg", "/static/images/css-100.svg"],
         live:"",
         code: "https://github.com/DiegoCoura/portfolio",
-    },
-    {
-        id: 2,
-        title: "Todo List",
-        banner: "/static/images/todo2.png",
-        description: "Challenging project using vanilla JavaScript, drag and drop, localStorage, sorting arrays and more",
-        technologies: ["/static/images/html-100.svg", "/static/images/css-100.svg", "/static/images/js-100.svg"],
-        live:"https://diegocoura.github.io/todo-list/",
-        code:"https://github.com/DiegoCoura/todo-list"
-    }, {
-        id: 3,
-        title: "AIMPROVER",
-        banner: "/static/images/aimprover-banner.png",
-        description: "A game to practice aiming for fps games",
-        technologies: ["/static/images/react-100.svg", "/static/images/css-100.svg"],
-        live: "https://diegocoura.github.io/aimprover/",
-        code: "https://github.com/DiegoCoura/aimprover"
     }, {
         id: 4,
         title: "Draw on the Wall",
